282c7e289c4042f8d65d849f912b86a7130157d9
Roman Bogdanov <romanbogdanov@MacBook-Pro-Roman.local>
2017-01-30 20:34:27 +0200

    fix-1: facebook auth

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,11 @@
 document.getElementById('loginBtn').addEventListener('click', function() {
     //do the login
     FB.login(function(response) {
-        if (response.authResponse) {
-            //user just authorized your app
-            top.location.href = 'success';
-        }
-    }, {scope: 'email,public_profile', return_scopes: true});
+        console.log(response);
+        if (response.status === 'connected') {
+            console.log('Logged into your app and Facebook.');
+        } else if (response.status === 'not_authorized') {
+          console.log('The person is logged into Facebook, but not your app.');
+        } 
+    }, {scope: 'public_profile,email'});
 }, false);
