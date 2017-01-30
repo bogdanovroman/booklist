941b785cd001153678bd7b71bfd3d82c0ddec04b
Roman Bogdanov <romanbogdanov@MacBook-Pro-Roman.local>
2017-01-30 20:12:35 +0200

    fix-1: facebook auth

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,7 +3,7 @@ document.getElementById('loginBtn').addEventListener('click', function() {
     FB.login(function(response) {
         if (response.authResponse) {
             //user just authorized your app
-            top.location.href = 'example.com/facebook_connect.php';
+            top.location.href = 'success';
         }
     }, {scope: 'email,public_profile', return_scopes: true});
 }, false);
