e83c73884f0956e91dc009c3943570826a60ca17
rbogdanov <romanlbogdanov@gmail.com>
2017-02-03 21:08:01 +0200

    fix fb

--- public/js/books/partials/header.jsx
+++ public/js/books/partials/header.jsx
@@ -29,7 +29,7 @@ export default class Header extends React.Component {
                               <span className="uk-text-middle">{this.state.name}</span>
                               <div className="uk-inline">
                                 <img className="uk-border-circle uk-margin-left" src={this.state.url} width="50" height="50"/>
-                                <div is uk-dropdown="mode: click, pos: bottom-right">
+                                <div is uk-dropdown>
                                     <ul className="uk-nav uk-dropdown-nav">
                                         <li><a href="#">Какая-то ссылка</a></li>
                                         <li><a href="#">Выйти</a></li>
